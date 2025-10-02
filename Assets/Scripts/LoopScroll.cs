d58275bf9aab3b4986309b4eef8a7acdb72075f2
DevCol <devcol.main@gmail.com>
2025-10-02 21:24:31 +0900

    Refactor background scrolling and add new environment assets
    Refactored LoopScroll.cs to clean up code and use FixedUpdate for smoother scrolling. Updated NormalScene.unity to reorganize background objects, adjust scroll speed, and remove unused background GameObject. Deleted LoopScrollWithRepos.cs and its meta file. Added new environment sprite assets and materials for backgrounds, ground types, and their associated meta files.

--- Assets/Scripts/LoopScroll.cs
+++ Assets/Scripts/LoopScroll.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using UnityEngine;
 
-// 2d object scroll
-
 [RequireComponent(typeof(SpriteRenderer))]
 public class LoopScroll : MonoBehaviour
 {
@@ -11,15 +9,11 @@ public class LoopScroll : MonoBehaviour
     [Range((float)0.5, (float)10.0)]
     [SerializeField] private float scrollSpeed;
 
-
     private SpriteRenderer spriteRenderer;
     private float width;
-
-    //private Vector2 startPosition;
     private float startPosition;
     private float endPosition;
 
-    //
     public bool IsScrollOn { get; set; }
 
     private void Awake()
@@ -27,53 +21,31 @@ public class LoopScroll : MonoBehaviour
         //
         spriteRenderer = this.GetComponent<SpriteRenderer>();
 
+        IsScrollOn = true;
+
+        CalculateWidth();
+
+    }
+    
+    private void CalculateWidth()
+    {
         width = spriteRenderer.size.x;
-
-        //
-
-        //startPosition = new Vector2(this.transform.position.x, this.transform.position.y);
-        
         int halfWidth = (int)(width * 0.5f);
 
         startPosition = halfWidth;
-
-        IsScrollOn = true;
-
-
-        print("startPosition: " + startPosition);
-
-        print("spriteRenderer: " + spriteRenderer.size.x);
-        // width = 25
-
-        // 25/2 = 12.5
-        // 12.5/4
-
         endPosition = -1 * halfWidth;
-
-        print("endPosition: " + endPosition);
-
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        // scroll left (Vector2.left == (-1,0)  )
+
         this.transform.Translate(Vector2.left * scrollSpeed * Time.deltaTime);
 
         // if at end position, reposition to start position
         if (this.transform.position.x < endPosition)
         {
-            //this.transform.position = new Vector2(startPosition.x, this.transform.position.y);
              this.transform.position = new Vector2(startPosition, this.transform.position.y);
         }
-
-        /*
-
-
-        if (this.transform.position.x < startPosition.x - (width * 0.5f))
-        {
-            this.transform.position = new Vector2(startPosition.x + (width * 0.5f), this.transform.position.y);
-        }
-        */
     }
 
 }
