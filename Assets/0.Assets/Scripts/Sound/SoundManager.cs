2382432b5c1658f2f632496d691e8b02a4e05193
DevCol <devcol.main@gmail.com>
2025-11-02 00:51:01 +0900

    Add Demo-StartButton prefab and update Normal scene
    Added Demo-StartButton - Play prefab and integrated it into the Normal.unity scene. Updated UI elements and sound asset management, including changes to SoundManager and SoundUIController components. Removed obsolete Sound Asset GameObject and improved scene organization for start button functionality.

--- Assets/0.Assets/Scripts/Sound/SoundManager.cs
+++ Assets/0.Assets/Scripts/Sound/SoundManager.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 using UnityEngine.Audio;
 
 //using UnityEngine.Audio;
+
+
 public class SoundManager : MonoBehaviour
 {
     // 
     public static SoundManager Instance;
 
-    [SerializeField] private AudioMixer audioMixer;
-
-    //public
+    // It brings erros on unity edior
+    //[SerializeField] private AudioMixer audioMixer;
+    public AudioMixer audioMixer;
+    
+    //
+    private SoundAsset soundAsset;
 
     private void Awake()
     {
+        soundAsset = FindAnyObjectByType<SoundAsset>();    
+
+
         if (Instance != null && Instance != this)
         {
             //Destroy(gameObject);
@@ -30,6 +38,66 @@ public class SoundManager : MonoBehaviour
         }
     }
 
+    // testing
+    private void Start()
+    {
+        // creating BGM audiosource child under this gameobject(sound manager)
+
+        print("BGM");
+        GameObject soundGameObject = new GameObject("Created BGM TEst");
+        soundGameObject.transform.SetParent(this.gameObject.transform);
+        AudioSource audioSource = soundGameObject.AddComponent<AudioSource>();
+
+        audioSource.clip = soundAsset.bgmSoundAudioClipArray[0].audioClip;
+        audioSource.playOnAwake = true;
+        audioSource.loop = true;
+        audioSource.spatialBlend = 0f;
+        audioSource.volume = 0.8f;
+
+        audioSource.outputAudioMixerGroup = audioMixer.FindMatchingGroups("BGM")[0];
+
+
+
+        audioSource.Play();
+        //Destroy(soundGameObject, logoBgmAudioClip.length);   
+    }
+
+    /*
+    public void PlayBgm(SoundAsset.BGM BgmName)
+    {
+        foreach (var bgmSound in soundAsset.bgmSoundAudioClipArray)
+        {
+            if (bgmSound.bgmName == BgmName)
+            {
+                //print(bgmSound.bgmName);
+
+                if (audioSource.isPlaying && (bgmSound.audioClip == audioSource.clip))
+                {
+                    print("from BGM: " + bgmSound.audioClip + " is on play returned");
+
+
+
+                    return;
+                }
+                else
+                {
+                    audioSource.clip = bgmSound.audioClip;
+
+
+                    audioSource.Play();
+                }
+
+
+
+                return;
+            }
+        }
+    }
+    */
+}
+
     
 
-}
+
+
+
